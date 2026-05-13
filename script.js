7d150e176d893f3c4e8c8038f8b3b17f19074a84
Mizráim Nascimento <mizraimfnascimento@gmail.com>
2026-05-12 22:20:26 -0300

    aula variáveis e constantes

---
+++ script.js
@@ -0,0 +1,10 @@
+let poteCafe = "café pilão"
+let poteAcucar = "Açúcar cristal"
+let poteBiscoito = "Biscoito Maizena"
+
+console.log("Na cozinha da vovó hoje tem: " + 
+poteCafe + " - " +
+poteAcucar + " - " +
+poteBiscoito + " - "
+)
+
