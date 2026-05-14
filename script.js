bcf4ef4646ff68f5f2edc016eeacb989a75c4237
Mizráim Nascimento <mizraimfnascimento@gmail.com>
2026-05-14 16:43:55 -0300

    Aula de variáveis, tipos de variáveis

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let poteCafe = "café pilão"
-let poteAcucar = "Açúcar cristal"
+let poteAcucar = "Açucar cristal"
 let poteBiscoito = "Biscoito Maizena"
+const menssagemDaVovó = "Na cozinha da vovó hoje tem: "
+
+console.log(menssagemDaVovó + 
+poteCafe + " - " +
+poteAcucar + " - " +
+poteBiscoito + " - "
+)
+
+poteCafe =" Café 3 corações"
 
 console.log("Na cozinha da vovó hoje tem: " + 
 poteCafe + " - " +
@@ -8,3 +17,32 @@ poteAcucar + " - " +
 poteBiscoito + " - "
 )
 
+//tipos de variáveis; 
+//pokemon
+let nomePokemon = "pikachu"
+let nivelPokemon = 20
+let pontosDeVida = 45
+let pokemonSexo = 'M'
+let selecionavel = true
+
+// nome, idade, número de telefone, cpf, endereço, recebe pensão
+// string = armazenar texto
+// number = armazenar números
+// boolean = armazenar valores lógicos
+
+let nomePessoa = "Eliete Cardoso"
+let idade = 54
+let numeroTelefone = "(11)977778888"
+let cpf = "123.456.789-00"
+let endereço = Rua Juventus, número 75 , bairro Mooca
+let recebePensao = true
+
+
+
+//Produto, Marca, modelo, cor, tipo de memoria, capacidade camera
+let produto = "telefone celular"
+let marca = "Xiaomi"
+let modelo = "Redmi Note 15 PRO"
+let cor = "Glacier Blue"
+let tipoMemoria = "8GB RAM"
+let capacidadeCamera = "200MP ultimate-claruty camera"
